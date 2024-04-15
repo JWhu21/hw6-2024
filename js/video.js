c4babb5ce2cf9aeae38d77643da1a5057b4a7969
Jiawei Hu <108969035+JWhu21@users.noreply.github.com>
2024-04-14 21:48:25 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,9 +1,3 @@
-var video;
-
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
-});
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
